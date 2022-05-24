a2cba13133360cfb932129375666e9642d4e0455
Granato Alessandro [e5032] <alessandro.granato@IGT.com>
2022-05-24 08:09:45 +0200

    3 Dart shots feature - Add possibility to insert 3 dart shot points when clicking Throw btn

--- lib/widgets/throw_darts.dart
+++ lib/widgets/throw_darts.dart
@@ -11,14 +11,25 @@ class ThrowDarts extends StatefulWidget {
 }
 
 class _ThrowDartsState extends State<ThrowDarts> {
-  final TextEditingController _pointsController = TextEditingController();
+  final TextEditingController _pointsController1 = TextEditingController();
+  final TextEditingController _pointsController2 = TextEditingController();
+  final TextEditingController _pointsController3 = TextEditingController();
 
   void _submitData() {
-    if (_pointsController.text.isEmpty) {
+    if (_pointsController1.text.isEmpty && _pointsController2.text.isEmpty && _pointsController3.text.isEmpty) {
       return;
     }
+    if (_pointsController1.text.isEmpty) {
+      _pointsController1.text = '0';
+    }
+    if (_pointsController2.text.isEmpty) {
+      _pointsController2.text = '0';
+    }
+    if (_pointsController3.text.isEmpty) {
+      _pointsController3.text = '0';
+    }
 
-    int points = int.parse(_pointsController.text);
+    int points = int.parse(_pointsController1.text) + int.parse(_pointsController2.text) + int.parse(_pointsController3.text);
     widget.addPointsFunction(widget.playerId, points);
     Navigator.of(context).pop();
   }
@@ -36,30 +47,51 @@ class _ThrowDartsState extends State<ThrowDarts> {
           right: 10,
           bottom: mediaQuery.viewInsets.bottom + 10,
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              child: Text("Add throw result", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline6),
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Darts points',
+        child: Container(
+          height: mediaQuery.size.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width,
+                child: Text("Add throw result", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headline6),
               ),
-              controller: _pointsController,
-              keyboardType: TextInputType.number,
-              onFieldSubmitted: (_) => _submitData(),
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Theme.of(context).colorScheme.primary,
-                  onPrimary: Theme.of(context).textTheme.button?.color),
-              onPressed: _submitData,
-              child: const Text('insert points'),
-            ),
-          ],
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Darts points',
+                ),
+                controller: _pointsController1,
+                keyboardType: TextInputType.number,
+                onFieldSubmitted: (_) {},
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Darts points',
+                ),
+                controller: _pointsController2,
+                keyboardType: TextInputType.number,
+                onFieldSubmitted: (_) {},
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Darts points',
+                ),
+                controller: _pointsController3,
+                keyboardType: TextInputType.number,
+                onFieldSubmitted: (_) {},
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Theme.of(context).colorScheme.primary,
+                    onPrimary: Theme.of(context).textTheme.button?.color),
+                onPressed: _submitData,
+                child: const Text('insert points'),
+              ),              
+            ],
+          ),
         ),
       ),
     );
